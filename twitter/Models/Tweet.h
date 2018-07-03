5b7d018ffb28843a927b2c61e348c6f0995e6e34
Jessica Shu <jessicashu7@fb.com>
2018-07-03 10:59:48 -0700

    implemented data models for User and Tweets

---
+++ twitter/Models/Tweet.h
@@ -0,0 +1,32 @@
+//
+//  Tweet.h
+//  twitter
+//
+//  Created by Jessica Shu on 7/2/18.
+//  Copyright © 2018 Emerson Malca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "User.h"
+
+@interface Tweet : NSObject
+
+// MARK: Properties
+@property (nonatomic, strong) NSString *idStr;
+@property (strong, nonatomic) NSString *text;
+@property (nonatomic) int favoriteCount;
+@property (nonatomic) BOOL favorited;
+@property (nonatomic) int retweetCount;
+@property (nonatomic) BOOL retweeted;
+@property (strong, nonatomic) User *user;
+@property (strong, nonatomic) NSString *createdAtString;
+
+// For Retweets
+@property (strong, nonatomic) User *retweetedByUser;
+
+-(instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
++ (NSMutableArray *)tweetsWithArray:(NSArray *)dictionaries;
+    
+@end
+
