fa683b317ef2235e9c16603e22d2b7101bf4d512
Jessica Shu <jessicashu7@fb.com>
2018-07-03 17:05:44 -0700

    ability to open and close compose tweet view controller

---
+++ twitter/View Controllers/ComposeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ComposeViewController.h
+//  twitter
+//
+//  Created by Jessica Shu on 7/3/18.
+//  Copyright © 2018 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ComposeViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextView *ComposeTextView;
+@end
